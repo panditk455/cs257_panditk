bb02916f688d400158568b59452803e8f99fa6b2
panditk455 <panditk@carleton.edu>
2024-01-29 09:33:00 -0600

    Added 2nd query

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -1,6 +1,7 @@
-# We use the psycopg2 library to help us connec to the Postgres SQL database
-# This library is already installed on stearns.mathcs.carleton.edu
+# These are different queries which does specifc function as given:
+
 import psycopg2
+
 
 # This function tests to make sure that you can connect to the database
 def test_connection():
@@ -19,7 +20,9 @@ def test_connection():
 
     return None
 
-# This function sends an SQL query to the database
+# This Determine if Northfield is present in the database. 
+# If it is, print its location (Latitude and Longitude).
+#  If it is not, print an appropriate message for the user.
 def test_query_one():
 
     conn = psycopg2.connect(
@@ -27,7 +30,8 @@ def test_query_one():
         port=5432,
         database="panditk",
         user="panditk",
-        password="square555cow")
+        password="square555cow"
+        )
 
     cur = conn.cursor()
 
@@ -38,31 +42,205 @@ def test_query_one():
     """   
     
     cur.execute( firstsql )
-
-    
     row = cur.fetchone()
 
     if row:
         print(f"Northfield is present in the dataase, where Location: Latitude {row[1]}, Longitude {row[2]}")
-
     else:
         print(f"Sorry, Northfield is not present in the database")
 
-
-    conn.commit()
+    cur.close()
+    conn.close()
     
 test_query_one()
 
-# def test_query_two():
+# This function will help to print out the name of the city with the largest population.
+def test_query_two():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="panditk",
+        user="panditk",
+        password="square555cow")
+
+    cur = conn.cursor()
+
+    secondsql = """
+    SELECT city, 
+    FROM cities 
+    ORDER BY population DESC
+    LIMIT 1"""
+    
+    cur.execute( secondsql )
+
+
+    # fetchone Just retuns one row:
+    row = cur.fetchone()
+
+    if row:
+        print(f"The city with lagest population:{row[0]} ")
+    else:
+        print("Opps! Something went wrong")
+
+    cur.close()
+    conn.close()
+    
+
+test_query_two()
+
+
+
+# # This function will help to print out the name of the city with the largest population.
+# def test_query_three():
+    
 #     conn = psycopg2.connect(
 #     host="localhost",
 #     port=5432,
 #     database="panditk",
 #     user="panditk",
-#     password="square555cow")
+#     password="square555cow"
+#     )
 
 #     cur = conn.cursor()
 
-#     secondsql = 
+#     thirdsql = """
+#     SELECT city,
+#     FROM cities
+#     WHERE state = 'Minnesota' 
+#     ORDER BY population 
+#     LIMIT 1"""
+    
+#     cur.execute( thirdsql )
+    
+#     # fetchone Just retuns one row:
+#     row = cur.fetchone()
 
-#     cur.execute( secondsql )
+#     if row:
+#         print(f"The city with smallest population in Minnesota is: {row[0]} ")
+#     else:
+#         print(f"No city in Minnesota with the smallest population")
+
+#     cur.close()
+#     conn.close()
+
+# test_query_three()
+
+
+# # This function will help to print out the name of the city with the largest population.
+# def test_query_four():
+
+#     conn = psycopg2.connect(
+#         host="localhost",
+#         port=5432,
+#         database="panditk",
+#         user="panditk",
+#         password="square555cow")
+
+#     cur = conn.cursor()
+    
+
+#     # For the state in the farthest east:
+#     eastsql = """
+#     SELECT city
+#     FROM cities
+#     ORDER BY lon DESC
+#     LIMIT 1
+#     """
+    
+#     cur.execute( eastsql )
+#     eastestcity = cur.fetchone()[0] if cur.rowcount > 0 else "No cities were found."
+
+#     # For the city in the farthest west:
+#     westsql = """
+#     SELECT city
+#     FROM cities
+#     ORDER BY lon ASC
+#     LIMIT 1
+#     """
+    
+#     cur.execute( westsql )
+#     westestcity = cur.fetchone()[0] if cur.rowcount > 0 else "No cities were found."
+
+#     # For the one in the farthest north:
+#     northsql = """
+#     SELECT city
+#     FROM cities
+#     ORDER BY lat DESC
+#     LIMIT 1;
+#     """
+
+#     cur.execute( northsql )
+#     northestcity = cur.fetchone()[0] if cur.rowcount > 0 else "No cities were found."
+
+
+#     # For the one in the farthest south:
+#     southsql = """
+#     SELECT city
+#     FROM cities
+#     ORDER BY lat ASC
+#     LIMIT 1;
+#     """
+
+#     cur.execute( southsql )
+#     southestcity = cur.fetchone()[0] if cur.rowcount > 0 else "No cities were found."
+    
+#     print (f"Furthest North: {northestcity}")
+#     print (f"Furthest East: {eastestcity}")
+#     print (f"Furthest West: {westestcity}")
+#     print (f"Furthest South: {southestcity}")
+    
+
+#     cur.close()
+#     conn.close()
+
+# test_query_four()
+
+
+# def test_query_five():
+
+#     conn = psycopg2.connect(
+#         host="localhost",
+#         port=5432,
+#         database="panditk",
+#         user="panditk",
+#         password="square555cow")
+
+#     cur = conn.cursor()
+
+#     input_state = input("Enter a State name, you can enter an Abbreviation or full name:")
+
+#     abbsql = """
+#     SELECT state
+#     FROM stateabb
+#     WHERE abbreviation = %s
+#     """
+    
+#     cur.execute(abbsql, (input_state,))
+    
+#     state_name = cur.fetchone()
+
+#     if not state_name:
+#         statename = (input_state,)
+
+    
+#     population_sql = """
+#     SELECT SUM(population)
+#     FROM cities 
+#     WHERE state = 
+#     """
+
+#     cur.execute(population_sql, (state_name,))
+
+#     total_population = cur.fetchone()[0] or 0
+
+#     print("The total population of {state_name}, including all cities is :{total_population}")
+
+
+#     cur.close()
+#     conn.close()
+
+# test_query_five()   
+
+
+
