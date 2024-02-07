d9d9c507bc7fffc5e7759d43a85005d22812b32b
panditk455 <panditk@carleton.edu>
2024-02-07 09:16:16 -0600

    few changes

--- flasklab/flask_test.py
+++ flasklab/flask_test.py
@@ -17,5 +17,5 @@ def my_color(word1):
     return '<h1 style="color:Red">' + word1 + '</h1>'
 
 if __name__ == '__main__':
-    my_port = 51315231
+    my_port = 5131
     app.run(host='0.0.0.0', port = my_port) 
