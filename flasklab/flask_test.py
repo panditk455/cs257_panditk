a5ce3c3bb1958a9324c11be408930cd3165465c7
panditk455 <panditk@carleton.edu>
2024-02-09 08:49:54 -0600

    Added the abbreviation

--- flasklab/flask_test.py
+++ flasklab/flask_test.py
@@ -1,4 +1,5 @@
 import flask
+import psycopg2
 
 app = flask.Flask(__name__)
 
@@ -21,9 +22,43 @@ def my_color(word1):
 def my_sum(num1, num2):
     sum = int(num1) + int(num2)
     return str(sum)
+
+@app.route('/pop/abbrev')
+def my_pop(pop, abbrev):
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="panditk",
+        user="panditk",
+        password="square555cow")
+
+    cur = conn.cursor()
     
+    abbsql = """
+    SELECT state
+    FROM stateabb
+    WHERE abbreviation = %s
+    """
 
+    cur.execute(abbsql, (abbrev,))
 
+    state_name = cur.fetchone()
+
+    if not state_name:
+        state_name = (abbrev,)
+
+    pop_sql = """SELECT SUM(population)
+    FROM cities 
+    WHERE state = %s
+    """
+    cur.execute(pop_sql, state_name)  # Passing state_name as a tuple
+
+    total_population = cur.fetchone()[0] or 0
+
+    return total_population
+
+    
 
 if __name__ == '__main__':
     my_port = 5131
