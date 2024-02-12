20fef3b82552e74ffdf797d60688c39f926fe4d5
panditk455 <panditk@carleton.edu>
2024-02-11 22:34:22 -0600

    Made some changes to the python file

--- flasklab/friday-flask-example/template-test.py
+++ flasklab/friday-flask-example/template-test.py
@@ -1,8 +1,13 @@
 import random
 
+import psycopg2
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+names = ["Kritika", "Will", "Kripa", "Navin", "Heidi", "Daniel", "Jeebika", "Darshan"]
+adjectives = ["nice", "awesome", "good", "kind", "horrible", "honest", "punctual"]
+years = list(range(1992, 2024))
 
 @app.route('/')
 def welcome():
@@ -10,14 +15,40 @@ def welcome():
 
 @app.route('/rand/<low>/<high>')
 def rand(low, high):
-    #Input values that come from a URL (i.e., @app.route)
-    #   are always strings so I need to convert the type to int
     low_int = int(low)
     high_int = int(high)
-    
     num = random.randint(low_int, high_int)
-    return render_template("random.html", randNum = num)
+    return render_template("random.html", randNum=num)
+
+@app.route('/randsentence')
+def randsentence():
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="panditk",
+        user="panditk",
+        password="square555cow")
+    
+    cur = conn.cursor()
+    
+    my_sql = """
+    SELECT city
+    FROM cities
+    ORDER BY random()
+    LIMIT 1"""
+
+    cur.execute(my_sql)
+    city = cur.fetchone()[0]  # Corrected variable name
+    cur.close()
+    
+    name = random.choice(names)
+    adjective = random.choice(adjectives)
+    year = random.choice(years)
+
+    required_sentence = "{name} the {adjective} was born in {city} in {year}" 
+
+    return render_template('index.html', required_sentence=required_sentence)  
 
 if __name__ == '__main__':
     my_port = 5131
-    app.run(host='0.0.0.0', port = my_port) 
+    app.run(host='0.0.0.0', port=my_port)
