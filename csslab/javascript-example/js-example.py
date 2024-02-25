13837e33ebd63b61498b6b8a2b1920a3e7573b93
panditk455 <panditk@carleton.edu>
2024-02-25 16:02:28 -0600

    Added css lab for class

--- csslab/javascript-example/js-example.py
+++ csslab/javascript-example/js-example.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,5 +9,5 @@ def welcome():
     return render_template("homepage.html", someText = message)
 
 if __name__ == '__main__':
-    my_port = 5000
+    my_port = 5131
     app.run(host='0.0.0.0', port = my_port) 
